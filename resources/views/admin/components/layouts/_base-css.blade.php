187c2dce798e6fca123ff6aa2758c2146bc6db96
wolllll <sakin50guramu@gmail.com>
2020-12-12 22:14:29 +0900

    npmのadminlte導入

--- resources/views/admin/components/layouts/_base-css.blade.php
+++ resources/views/admin/components/layouts/_base-css.blade.php
@@ -1,6 +1,4 @@
 {{-- Font Awesome --}}
 <link rel="stylesheet" href="{{ vasset('vendor/plugins/fontawesome-free/css/all.css') }}">
-{{-- admin-lte3 --}}
-<link rel="stylesheet" href="{{ vasset('vendor/AdminLTE/css/adminlte.css') }}">
 {{-- original --}}
 <link rel="stylesheet" href="{{ vasset('assets/admin/css/app.css') }}">
--- resources/views/admin/components/layouts/_base-js.blade.php
+++ resources/views/admin/components/layouts/_base-js.blade.php
@@ -1,6 +1,2 @@
-<!-- jQuery -->
-<script src="{{ vasset('vendor/plugins/jquery/jquery.js') }}"></script>
-{{-- admin-lte3 --}}
-<script src="{{ vasset('vendor/AdminLTE/js/adminlte.js') }}"></script>
 <!-- original -->
 <script src="{{ vasset('assets/admin/js/app.js') }}"></script>
